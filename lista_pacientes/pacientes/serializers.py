3140b59d05c2e207546b72c965e4ebb3296d3b81
francivaldo4334 <francivaldodev@gmail.com>
2024-01-04 15:50:27 -0300

    fix: serialiler add date create

--- lista_pacientes/pacientes/serializers.py
+++ lista_pacientes/pacientes/serializers.py
@@ -4,4 +4,4 @@ from .models import Paciente
 class PacienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Paciente
-        fields = ['id','nome','idade','status','sexo','preferencial']
+        fields = ['id','nome','idade','status','sexo','preferencial','criado_em']

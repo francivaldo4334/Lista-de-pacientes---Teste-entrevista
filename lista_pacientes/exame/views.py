250105337bf48cf5e930c5e86035eebdb154d094
francivaldo4334 <francivaldodev@gmail.com>
2024-01-05 10:29:25 -0300

    feat: add get exames

--- lista_pacientes/exame/views.py
+++ lista_pacientes/exame/views.py
@@ -1,7 +1,14 @@
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Exame
+from .serializers import ExameSerializer
 
 # Create your views here.
 class ExameList(APIView):
     def get(self,request):
-        pass
+        exaem_list = Exame.objects.all()
+        serializer_exame_list = ExameSerializer(exaem_list,many=True)
+        return Response(
+            serializer_exame_list.data,
+            status=200
+        )
